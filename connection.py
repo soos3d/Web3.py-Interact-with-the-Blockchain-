3a6f39b1081261822e111a33e3ec8348fed7b5bd
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-04-17 22:11:22 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
-node_url = 'https://nd-487-536-496.p2pify.com/9bf8d3c8e5a35772710aad15a469f933'    # mainnet endpoint URL
-web3 = Web3(Web3.HTTPProvider(node_url))                                           # establish connection to a ETH mainnet node
+node_url = 'YOUR NODE URL'                                          # mainnet endpoint URL
+web3 = Web3(Web3.HTTPProvider(node_url))                            # establish connection to a ETH mainnet node
 
 # verify the connection is successful (will return true)
 if web3.isConnected():                                                          
@@ -13,7 +13,7 @@ else:
 print('The latest block number is: ', web3.eth.blockNumber)                     
 
 # return an address balance in wei
-balance = web3.eth.get_balance('0x88E0204C86ddA961dAcC231Ec275ad095382d61B')    
+balance = web3.eth.get_balance('0x88E0204C86ddA961dAcC231Ec275ad095382d61B')    #random address found on BSCScan
 
 # converts from wei to eth
 weiToEth = web3.fromWei(balance , 'ether')                              
@@ -29,7 +29,7 @@ for hash in pending_tx:
     print('Hash of a Pending Transaction:' , web3.toHex(hash))      # web3.toHex converts from bytes to hex
 
 #  extract data from the hash                                                         
-details = web3.eth.getTransaction('0x0572d76caf0b37be8021d84d6fccdcc466824b8e9dce68eef079f5e5d055139e')
+details = web3.eth.getTransaction('0x0572d76caf0b37be8021d84d6fccdcc466824b8e9dce68eef079f5e5d055139e')         #random hash found on BSCScan
 #print(details)
 
 # convert the web3 object into a dictionary that python can read
